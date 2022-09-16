cb2868f6a43637395691550e9f171cd8ead6ec9f
Roman Simuta <simutaroman@gmail.com>
2022-09-16 20:25:01 +0300

    Tutorials cleanup

--- Tutorials/GettingStarted/Server/Program.cs
+++ Tutorials/GettingStarted/Server/Program.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using Blazor3DGettingStartedSrv.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
